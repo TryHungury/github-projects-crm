aa1a0752dd9b25bfecc16ad2e7fc9ce4f3485a13
Bohdan <105644270+TryHungury@users.noreply.github.com>
2025-03-27 15:47:41 +0200

    fix: back up to before changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
 // src/App.tsx
-import { Routes, Route } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <HashRouter basename="/github-projects-crm/">
+    <BrowserRouter basename="/github-projects-crm/">
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
