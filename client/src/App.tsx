5035daeea51910ad105b39cbc8dd8da03e4aab76
Bohdan <105644270+TryHungury@users.noreply.github.com>
2025-03-27 15:25:56 +0200

    fix: add HashRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <BrowserRouter basename="/github-projects-crm/">
+    <HashRouter basename="/github-projects-crm/">
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
